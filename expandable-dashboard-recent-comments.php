af8c39e053c6900ee6cee8d52363ff30d8a282f3
coffee2code <coffee2code@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-08-27 22:18:21 +0000

    * Update to v1.1 * Add filter 'c2c_expandable_dashboard_recent_comments_config' to allow filtering of config options * Rename class from 'ExpandableDashboardRecentComments' to 'c2c_ExpandableDashboardRecentComments' * Store plugin instance in global variable, $c2c_expandable_dashboard_recent_comments, to allow for external manipulation * Move is_admin() check to before class creation * Add init() and move hooking of actions/filters to there * Remove docs from top of plugin file (all that and more are in readme.txt) * Note compatibility with WP 3.0+ * Minor code reformatting (spacing) * Add Filters and Upgrade Notice sections to readme * Remove trailing whitespace in header docs
    git-svn-id: https://plugins.svn.wordpress.org/expandable-dashboard-recent-comments/tags/1.1@281726 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- expandable-dashboard-recent-comments.php
+++ expandable-dashboard-recent-comments.php
@@ -2,47 +2,30 @@
 /**
  * @package Expandable_Dashboard_Recent_Comments
  * @author Scott Reilly
- * @version 1.0.1
+ * @version 1.1
  */
 /*
 Plugin Name: Expandable Dashboard Recent Comments
-Version: 1.0.1
-Plugin URI: http://coffee2code.com/wp-plugins/expandable-dashboard-recent-comments
+Version: 1.1
+Plugin URI: http://coffee2code.com/wp-plugins/expandable-dashboard-recent-comments/
 Author: Scott Reilly
 Author URI: http://coffee2code.com
 Description: Adds the ability to do an in-place expansion of comment excerpts on the admin dashboard 'Recent Comments' widget.
 
-By default, the 'Recent Comments' admin dashboard widget only shows an excerpt for the comments, truncating the content of the
-comments to the first 20 words while at the same time stripping out all markup.
+Compatible with WordPress 2.6+, 2.7+, 2.8+, 2.9+, 3.0+.
 
-This plugin adds a link at the end of the comment excerpt (a ">>" (aka &raquo;)) that when clicked will replace the excerpt with
-the full comment.  The full comment will include all markup, including originally utilized markup and changes applied via filters,
-plugins, etc (such as shortcode expansion, smilies, paragraphing, etc).  The full comment can be switched back to the except by
-clicking a "<<" (&laquo;) link.
+=>> Read the accompanying readme.txt file for instructions and documentation.
+=>> Also, visit the plugin's homepage for additional information and updates.
+=>> Or visit: http://wordpress.org/extend/plugins/hide-broken-shortcodes/
 
-"In-place expansion" refers to the ability to click the link to see the full comment and it will be presented in place of the
-excerpt without requiring a page reload or navigation.
-
-NOTE: This plugin only works for users who have JavaScript enabled.
-
-Compatible with WordPress 2.6+, 2.7+, 2.8+, 2.9+.
-
-=>> Read the accompanying readme.txt file for more information.  Also, visit the plugin's homepage
-=>> for more information and the latest updates
-
-Installation:
-
-1. Download the file http://coffee2code.com/wp-plugins/expandable-dashboard-recent-comments.zip and unzip it into your 
-/wp-content/plugins/ directory (or install via the built-in WordPress plugin installer).
-2. Activate the plugin through the 'Plugins' admin menu in WordPress
 */
 
 /*
 Copyright (c) 2009-2010 by Scott Reilly (aka coffee2code)
 
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation 
-files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, 
-modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the 
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy,
+modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the
 Software is furnished to do so, subject to the following conditions:
 
 The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
@@ -53,8 +36,9 @@ LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRA
 IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
-if ( !class_exists('ExpandableDashboardRecentComments') ) :
-class ExpandableDashboardRecentComments {
+if ( is_admin() && !class_exists( 'c2c_ExpandableDashboardRecentComments' ) ) :
+class c2c_ExpandableDashboardRecentComments {
+	// This just defines the default config. Values can be filtered via the filter 'c2c_expandable_dashboard_recent_comments_config'
 	var $config = array(
 		'remove-ellipsis' => false,
 		'more-text' => ' &raquo;',
@@ -64,12 +48,19 @@ class ExpandableDashboardRecentComments {
 	/**
 	 * Class constructor: initializes class variables and adds actions and filters.
 	 */
-	function ExpandableDashboardRecentComments() {
+	function c2c_ExpandableDashboardRecentComments() {
 		global $pagenow;
-		if ( is_admin() && 'index.php' == $pagenow ) {
-			add_action('admin_head', array(&$this, 'add_css'));
-			add_filter('comment_excerpt', array(&$this, 'expandable_comment_excerpts'));
-		}
+		if ( 'index.php' == $pagenow )
+			add_action( 'admin_init', array( &$this, 'init' ) );
+	}
+
+	/**
+	 * Initialize the config and register actions/filters
+	 */
+	function init() {
+		$this->config = apply_filters( 'c2c_expandable_dashboard_recent_comments_config', $this->config );
+		add_action( 'admin_head', array( &$this, 'add_css' ) );
+		add_filter( 'comment_excerpt', array( &$this, 'expandable_comment_excerpts' ) );
 	}
 
 	/**
@@ -96,27 +87,26 @@ CSS;
 	 */
 	function expandable_comment_excerpts( $excerpt ) {
 		global $comment;
-		if ( preg_match('/\.\.\.$/', $excerpt) ) {
-			$body = apply_filters('comment_text', apply_filters('get_comment_text', $comment->comment_content), '40');
+		if ( preg_match( '/\.\.\.$/', $excerpt ) ) {
+			$body = apply_filters( 'comment_text', apply_filters( 'get_comment_text', $comment->comment_content ), '40' );
 			$class = "excerpt-{$comment->comment_ID}";
 			$extended = "<div class='{$class}-short excerpt-short'>" .
-				( $this->config['remove-ellipsis'] ? substr($excerpt, 0, -3) : $excerpt ) .
+				( $this->config['remove-ellipsis'] ? substr( $excerpt, 0, -3 ) : $excerpt ) .
 				"<a href='#' onclick=\"javascript:jQuery('.{$class}-short, .{$class}-full').toggle();return false;\" title='" .
-				__('Show full comment') . "'>{$this->config['more-text']}</a></div>" .
+				__( 'Show full comment' ) . "'>{$this->config['more-text']}</a></div>" .
 				"<div class='{$class}-full excerpt-full' style='display:none;'>" .
 				$body . 
 				" <a href='#' onclick=\"javascript:jQuery('.{$class}-full, .{$class}-short').toggle();return false;\" title='" .
-				__('Show excerpt') . "'>{$this->config['less-text']}</a></div>";
-			$excerpt = preg_replace('/\.\.\.$/', $excerpt, $extended);
+				__( 'Show excerpt' ) . "'>{$this->config['less-text']}</a></div>";
+			$excerpt = preg_replace( '/\.\.\.$/', $excerpt, $extended );
 		}
 		return $excerpt;
 	}
 
-} // end ExpandableDashboardRecentComments
+} // end c2c_ExpandableDashboardRecentComments
+
+$GLOBALS['c2c_expandable_dashboard_recent_comments'] = new c2c_ExpandableDashboardRecentComments();
 
 endif; // end if !class_exists()
-
-if ( class_exists('ExpandableDashboardRecentComments') )
-	new ExpandableDashboardRecentComments();
 
 ?>
