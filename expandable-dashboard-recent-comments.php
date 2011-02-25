5f4de5ee9e7fbdc5443cd9ea79567f680889759c
coffee2code <coffee2code@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-02-25 22:31:42 +0000

    * Update to v1.3 * Don't display expand/collapse links for users without JavaScript and jQuery enabled * Add admin unobtrusive javascript to handle expand/collapse of comments when links are clicked * Use substr() instead of preg_match() to detect presence of '...' * Remove 'onclick' attribute for links (perform via unobtrusive JS) * Fix plugin links in description in readme.txt
    git-svn-id: https://plugins.svn.wordpress.org/expandable-dashboard-recent-comments/tags/1.3@351447 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- expandable-dashboard-recent-comments.php
+++ expandable-dashboard-recent-comments.php
@@ -2,47 +2,33 @@
 /**
  * @package Expandable_Dashboard_Recent_Comments
  * @author Scott Reilly
- * @version 1.0.1
+ * @version 1.3
  */
 /*
 Plugin Name: Expandable Dashboard Recent Comments
-Version: 1.0.1
-Plugin URI: http://coffee2code.com/wp-plugins/expandable-dashboard-recent-comments
+Version: 1.3
+Plugin URI: http://coffee2code.com/wp-plugins/expandable-dashboard-recent-comments/
 Author: Scott Reilly
 Author URI: http://coffee2code.com
 Description: Adds the ability to do an in-place expansion of comment excerpts on the admin dashboard 'Recent Comments' widget.
 
-By default, the 'Recent Comments' admin dashboard widget only shows an excerpt for the comments, truncating the content of the
-comments to the first 20 words while at the same time stripping out all markup.
+Compatible with WordPress 2.6+, 2.7+, 2.8+, 2.9+, 3.0+, 3.1+.
 
-This plugin adds a link at the end of the comment excerpt (a ">>" (aka &raquo;)) that when clicked will replace the excerpt with
-the full comment.  The full comment will include all markup, including originally utilized markup and changes applied via filters,
-plugins, etc (such as shortcode expansion, smilies, paragraphing, etc).  The full comment can be switched back to the except by
-clicking a "<<" (&laquo;) link.
+=>> Read the accompanying readme.txt file for instructions and documentation.
+=>> Also, visit the plugin's homepage for additional information and updates.
+=>> Or visit: http://wordpress.org/extend/plugins/hide-broken-shortcodes/
 
-"In-place expansion" refers to the ability to click the link to see the full comment and it will be presented in place of the
-excerpt without requiring a page reload or navigation.
+TODO:
+	* Make it possible for comments to start off expanded rather than collapsed?
 
-NOTE: This plugin only works for users who have JavaScript enabled.
-
-Compatible with WordPress 2.6+, 2.7+, 2.8+, 2.9+.
-
-=>> Read the accompanying readme.txt file for more information.  Also, visit the plugin's homepage
-=>> for more information and the latest updates
-
-Installation:
-
-1. Download the file http://coffee2code.com/wp-plugins/expandable-dashboard-recent-comments.zip and unzip it into your 
-/wp-content/plugins/ directory (or install via the built-in WordPress plugin installer).
-2. Activate the plugin through the 'Plugins' admin menu in WordPress
 */
 
 /*
-Copyright (c) 2009-2010 by Scott Reilly (aka coffee2code)
+Copyright (c) 2009-2011 by Scott Reilly (aka coffee2code)
 
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation 
-files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, 
-modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the 
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy,
+modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the
 Software is furnished to do so, subject to the following conditions:
 
 The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
@@ -53,9 +39,11 @@ LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRA
 IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
-if ( !class_exists('ExpandableDashboardRecentComments') ) :
-class ExpandableDashboardRecentComments {
-	var $config = array(
+if ( is_admin() && !class_exists( 'c2c_ExpandableDashboardRecentComments' ) ) :
+
+class c2c_ExpandableDashboardRecentComments {
+	// This just defines the default config. Values can be filtered via the filter 'c2c_expandable_dashboard_recent_comments_config'
+	public static $config = array(
 		'remove-ellipsis' => false,
 		'more-text' => ' &raquo;',
 		'less-text' => ' &laquo;'
@@ -64,28 +52,72 @@ class ExpandableDashboardRecentComments {
 	/**
 	 * Class constructor: initializes class variables and adds actions and filters.
 	 */
-	function ExpandableDashboardRecentComments() {
+	public static function init() {
 		global $pagenow;
-		if ( is_admin() && 'index.php' == $pagenow ) {
-			add_action('admin_head', array(&$this, 'add_css'));
-			add_filter('comment_excerpt', array(&$this, 'expandable_comment_excerpts'));
-		}
+		if ( 'index.php' == $pagenow )
+			add_action( 'admin_init', array( __CLASS__, 'do_init' ) );
+	}
+
+	/**
+	 * Initialize the config and register actions/filters
+	 */
+	public static function do_init() {
+		self::$config = apply_filters( 'c2c_expandable_dashboard_recent_comments_config', self::$config );
+		add_action( 'admin_print_styles',         array( __CLASS__, 'add_css' ) );
+		add_action( 'admin_print_footer_scripts', array( __CLASS__, 'add_js' ) );
+		add_filter( 'comment_excerpt',            array( __CLASS__, 'expandable_comment_excerpts' ) );
 	}
 
 	/**
 	 * Echoes the CSS for this plugin within style tags
 	 *
 	 */
-	function add_css() {
+	public static function add_css() {
 		echo <<<CSS
 		<style type="text/css">
-		#the-comment-list .comment-item blockquote .excerpt-full p {
-			display:block;
-			margin:1em 0;
-		}
+		#the-comment-list .comment-item blockquote .excerpt-full p { display:block; margin:1em 0; }
+		#dashboard_recent_comments .excerpt-short a { display:none; }
 		</style>
 
 CSS;
+	}
+
+	/**
+	 * Returns class name to be used for specific comment
+	 *
+	 * @since 1.3
+	 * @param int|string|null The comment ID (or null to get the ID for the current comment)
+	 * @return string The class
+	 */
+	private static function get_comment_class( $comment_id = null ) {
+		if ( !$comment_id ) {
+			global $comment;
+			$comment_id = $comment->comment_ID;
+		}
+		return "excerpt-$comment_id";
+	}
+
+	/**
+	 * Echoes the JS for this plugin within script tags
+	 *
+	 * @since 1.3
+	 */
+	public static function add_js() {
+			echo <<<JS
+		<script type="text/javascript">
+		if (jQuery) {
+			jQuery(document).ready(function($) {
+				$('.excerpt-ellipsis').hide();
+				$('#dashboard_recent_comments div.excerpt-short a').show();
+				$('#dashboard_recent_comments div.excerpt-short a, #dashboard_recent_comments div.excerpt-full a').click(function() {
+					$(this).parent().parent().find('div.excerpt-short, div.excerpt-full').toggle();
+					return false;
+				})
+			});
+		}
+		</script>
+
+JS;
 	}
 
 	/**
@@ -94,29 +126,27 @@ CSS;
 	 * @param string $excerpt Excerpt
 	 * @return string The $excerpt modified to have show more/less links when applicable
 	 */
-	function expandable_comment_excerpts( $excerpt ) {
+	public static function expandable_comment_excerpts( $excerpt ) {
 		global $comment;
-		if ( preg_match('/\.\.\.$/', $excerpt) ) {
-			$body = apply_filters('comment_text', apply_filters('get_comment_text', $comment->comment_content), '40');
-			$class = "excerpt-{$comment->comment_ID}";
-			$extended = "<div class='{$class}-short excerpt-short'>" .
-				( $this->config['remove-ellipsis'] ? substr($excerpt, 0, -3) : $excerpt ) .
-				"<a href='#' onclick=\"javascript:jQuery('.{$class}-short, .{$class}-full').toggle();return false;\" title='" .
-				__('Show full comment') . "'>{$this->config['more-text']}</a></div>" .
+		if ( substr( $excerpt, -3 ) == '...' ) {
+			$body = apply_filters( 'comment_text', apply_filters( 'get_comment_text', $comment->comment_content ), '40' );
+			$class = self::get_comment_class( $comment->comment_ID );
+			$extended = self::$config['remove-ellipsis'] ? '<span class="excerpt-ellipsis">...</span>' : ''; // Will only be seen if JS is disabled
+			$extended .= "<div class='{$class}-short excerpt-short'>" .
+				( self::$config['remove-ellipsis'] ? substr( $excerpt, 0, -3 ) : $excerpt ) .
+				"<a href='#' title='" . __( 'Show full comment' ) . "'>" . self::$config['more-text'] . '</a></div>' .
 				"<div class='{$class}-full excerpt-full' style='display:none;'>" .
 				$body . 
-				" <a href='#' onclick=\"javascript:jQuery('.{$class}-full, .{$class}-short').toggle();return false;\" title='" .
-				__('Show excerpt') . "'>{$this->config['less-text']}</a></div>";
-			$excerpt = preg_replace('/\.\.\.$/', $excerpt, $extended);
+				" <a href='#' title='" . __( 'Show excerpt' ) . "'>" . self::$config['less-text'] . '</a></div>';
+			$excerpt = preg_replace( '/\.\.\.$/', $excerpt, $extended );
 		}
 		return $excerpt;
 	}
 
-} // end ExpandableDashboardRecentComments
+} // end c2c_ExpandableDashboardRecentComments
+
+c2c_ExpandableDashboardRecentComments::init();
 
 endif; // end if !class_exists()
-
-if ( class_exists('ExpandableDashboardRecentComments') )
-	new ExpandableDashboardRecentComments();
 
 ?>
